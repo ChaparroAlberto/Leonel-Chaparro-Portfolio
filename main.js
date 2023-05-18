7f0e8f55ad7b01722fe4d298a28d9201f470846f
ChaparroAlberto <leoch46@hotmail.com.ar>
2023-05-18 10:37:18 -0700

    Subida del portfolio

---
+++ main.js
@@ -0,0 +1,88 @@
+window.addEventListener('scroll', function(){
+    var header = document.querySelector('header');
+    header.classList.toggle("sticky", window.scrollY > 0)
+})
+
+// SLIDER HOME
+// const slides = document.querySelectorAll(".slide");
+// const sliderdots = document.querySelectorAll(".slider-dot");
+
+// sliderdots.forEach((dot, i) => {
+//   dot.addEventListener("click", () => {
+//     let position = i;
+//     let operation = position * -100;
+
+//     slides.forEach((slide) => {
+//       slide.style.transform = `translateX(${operation}%)`;
+//     });
+
+//     sliderdots.forEach((dot) => {
+//       dot.classList.remove("active");
+//     });
+//     sliderdots[i].classList.add("active");
+//   });
+// });
+
+const slides = document.querySelectorAll(".slide");
+const sliderdots = document.querySelectorAll(".slider-dot");
+
+let slideIndex = 0;
+const intervalTime = 5000; // Intervalo de tiempo en milisegundos (ejemplo: 5000 = 5 segundos)
+let slideInterval;
+
+const nextSlide = () => {
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(-${slideIndex * 100}%)`;
+  });
+
+  sliderdots.forEach((dot) => {
+    dot.classList.remove("active");
+  });
+  sliderdots[slideIndex].classList.add("active");
+
+  slideIndex++;
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  }
+};
+
+const startSlide = () => {
+  slideInterval = setInterval(nextSlide, intervalTime);
+};
+
+const stopSlide = () => {
+  clearInterval(slideInterval);
+};
+
+sliderdots.forEach((dot, i) => {
+  dot.addEventListener("click", () => {
+    slideIndex = i;
+    stopSlide();
+    nextSlide();
+    startSlide();
+  });
+});
+
+startSlide();
+
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 100) / calcHeight);
+  if (pos > 100) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#40c2a6 ${scrollValue}%, #edeef0 ${scrollValue}%)`;
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
